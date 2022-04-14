662a34417fa92dc4120710e9cee3938728cb2b86
anupammish <mishra.anupam941@gmail.com>
2022-04-14 04:31:56 -0700

    third commit

--- demoproject/src/practiceProgram/CalculatorFunction.java
+++ demoproject/src/practiceProgram/CalculatorFunction.java
@@ -6,6 +6,10 @@ public class CalculatorFunction {
 
 	public static void main(String[] args) {
 		System.out.println("this is git demo");
+		System.out.println("here we do sum");
+		System.out.println("here we do diffrence");
+		System.out.println("here we do multiplication");
+		System.out.println("here we do division");
 	}
 
 }
